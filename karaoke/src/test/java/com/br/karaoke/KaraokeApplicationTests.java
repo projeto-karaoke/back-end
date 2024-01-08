05422d36ca346d6cbae00375305e8ae80f389536
heyanaleal <90358925+heyanaleal@users.noreply.github.com>
2024-01-08 19:50:03 -0300

    first commit

---
+++ karaoke/src/test/java/com/br/karaoke/KaraokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.karaoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaraokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
